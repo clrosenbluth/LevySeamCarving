a889e23f2165192f33f76f16eb453142c0ffce1c
clrosenbluth <clrosenbluth@gmail.com>
2022-12-14 22:26:42 -0500

    ditto

--- src/main/java/SeamRemover.java
+++ src/main/java/SeamRemover.java
@@ -7,7 +7,9 @@ public class SeamRemover
     // 2. create double[][] energy (from Color array)
     // 3. find lowest-energy vertical seam (from energy array)
     // 4. remove seam (from Color array)
-        // two copies of the array (row by row): everything before the seam, then everything after the seam. Use System.arrayCopy
+        // two copies of the array (row by row):
+        // everything before the seam, then everything after the seam.
+        // Use System.arrayCopy
     // 5. go to step 2
     // Repeat until width is as desired
 
