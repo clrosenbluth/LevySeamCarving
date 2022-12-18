ed93dc5d25f5ef5ab91619f61dd7c9996d3deb57
clrosenbluth <clrosenbluth@gmail.com>
2022-12-18 10:42:41 -0500

    added seamFinder tests, fixed seamFinder class

--- src/main/java/SeamFinder.java
+++ src/main/java/SeamFinder.java
@@ -14,7 +14,7 @@ public class SeamFinder
         int[] minSeam = new int[energy.length];
 
         double[] lastRow = new double[energy[0].length];
-        System.arraycopy(energy[energy.length - 1], 0, lastRow, 0, energy.length);
+        System.arraycopy(map[energy.length - 1], 0, lastRow, 0, energy[0].length);
 
         minSeam[energy.length - 1] = lastCoordinate(lastRow);
         for (int row = energy.length - 2; row >= 0; row--)
@@ -22,7 +22,7 @@ public class SeamFinder
             int bottomRow = minSeam[row + 1];
             int left = bottomRow == energy[0].length ? energy[0].length : bottomRow - 1;
             int right = bottomRow == 0 ? 0 : bottomRow + 1;
-            minSeam[row] = bottomRow + calculateOffset(energy[row][left], energy[row][bottomRow], energy[row][right]);
+            minSeam[row] = bottomRow + calculateOffset(map[row][left], map[row][bottomRow], map[row][right]);
         }
 
         return minSeam;
@@ -36,16 +36,16 @@ public class SeamFinder
         double[] lastCol = new double[energy.length];
         for (int row = 0; row < energy.length; row++)
         {
-            lastCol[row] = energy[row][energy[0].length - 1];
+            lastCol[row] = map[row][energy[0].length - 1];
         }
 
-        minSeam[energy.length - 1] = lastCoordinate(lastCol);
-        for (int col = energy.length - 2; col >= 0; col--)
+        minSeam[energy[0].length - 1] = lastCoordinate(lastCol);
+        for (int col = energy[0].length - 2; col >= 0; col--)
         {
             int rightCol = minSeam[col + 1];
             int top = rightCol == 0 ? 0 : rightCol - 1;
-            int bottom = rightCol == energy.length ? energy.length : rightCol + 1;
-            minSeam[col] = rightCol + calculateOffset(bottom, rightCol, top);
+            int bottom = rightCol == energy.length - 1 ? energy.length - 1 : rightCol + 1;
+            minSeam[col] = rightCol + calculateOffset(map[bottom][col], map[rightCol][col], map[top][col]);
         }
 
         return minSeam;
@@ -59,6 +59,7 @@ public class SeamFinder
         {
             if (finalEnergies[cell] < minEnergy)
             {
+                minEnergy = finalEnergies[cell];
                 minCoordinate = cell;
             }
         }
@@ -70,11 +71,11 @@ public class SeamFinder
         int offset = 0;
         if (leftOrBottom < same && leftOrBottom < rightOrTop)
         {
-            offset = -1;
+            offset = 1;
         }
         else if (rightOrTop < same && rightOrTop < leftOrBottom)
         {
-            offset = 1;
+            offset = -1;
         }
         return offset;
     }
@@ -83,16 +84,16 @@ public class SeamFinder
     {
         double[][] verticalEnergyMap = new double[energy.length][energy[0].length];
 
-        System.arraycopy(energy[0], 0, verticalEnergyMap[0], 0, verticalEnergyMap.length);
+        System.arraycopy(energy[0], 0, verticalEnergyMap[0], 0, verticalEnergyMap[0].length);
 
         for (int row = 1; row < verticalEnergyMap.length; row++)
         {
             int lastCell = verticalEnergyMap[0].length - 1;
 
-            verticalEnergyMap[row][0] = Math.min(verticalEnergyMap[row - 1][0],
-                                                 verticalEnergyMap[row - 1][1]);
-            verticalEnergyMap[row][lastCell] = Math.min(verticalEnergyMap[row - 1][lastCell],
-                                                        verticalEnergyMap[row - 1][lastCell]);
+            verticalEnergyMap[row][0] = Math.min(verticalEnergyMap[row - 1][0] + energy[row][0],
+                                                 verticalEnergyMap[row - 1][1] + energy[row][0]);
+            verticalEnergyMap[row][lastCell] = Math.min(verticalEnergyMap[row - 1][lastCell] + energy[row][lastCell],
+                                                        verticalEnergyMap[row - 1][lastCell - 1] + energy[row][lastCell]);
 
             for (int col = 1; col < lastCell; col++)
             {
@@ -122,10 +123,10 @@ public class SeamFinder
         {
             int lastCell = horizontalEnergyMap.length - 1;
 
-            horizontalEnergyMap[0][col] = Math.min(horizontalEnergyMap[0][col - 1],
-                                                   horizontalEnergyMap[1][col - 1]);
-            horizontalEnergyMap[lastCell][col] = Math.min(horizontalEnergyMap[lastCell][col - 1],
-                                                          horizontalEnergyMap[lastCell][col - 1]);
+            horizontalEnergyMap[0][col] = Math.min(horizontalEnergyMap[0][col - 1] + energy[0][col],
+                                                   horizontalEnergyMap[1][col - 1] + energy[0][col]);
+            horizontalEnergyMap[lastCell][col] = Math.min(horizontalEnergyMap[lastCell][col - 1] + energy[lastCell][col],
+                                                          horizontalEnergyMap[lastCell - 1][col - 1] + energy[lastCell][col]);
 
             for (int row = 1; row < lastCell; row++)
             {
---
+++ src/test/java/SeamFinderTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeamFinderTest
+{
+    SeamFinder seamFinder = new SeamFinder();
+    double[][] energy = new double[][] {
+            {1, 4, 3, 5, 2},
+            {3, 2, 5, 2, 3},
+            {5, 2, 4, 2, 1}
+    };
+
+    @Test
+    public void findVerticalSeam()
+    {
+        // Given
+        int[] anticipatedSeam = new int[] {0, 1, 1};
+
+        // When
+        int[] newSeam = seamFinder.findVerticalSeam(energy);
+
+        // Then
+        assertArrayEquals(anticipatedSeam, newSeam);
+
+    }
+
+    @Test
+    public void findHorizontalSeam()
+    {
+        // Given
+        int[] anticipatedSeam = new int[] {0, 1, 0, 1, 2};
+
+        // When
+        int[] newSeam = seamFinder.findHorizontalSeam(energy);
+
+        // Then
+        assertArrayEquals(anticipatedSeam, newSeam);
+    }
+
+}
